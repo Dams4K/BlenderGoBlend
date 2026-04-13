34c98d3ad17a2343f1d6ee64100d1c59cfd6be15
Dams4K <dams4k@users.noreply.github.com>
2026-04-14 00:07:30 +0200

    export operator (wyp)

--- __init__.py
+++ __init__.py
@@ -1,12 +1,17 @@
 import bpy
 
-# from properties import Property
+bl_info = {
+    "name": "GoBlend",
+    "author": "Dams4K",
+    "version": (1, 0),
+    "blender": (5, 1, 0)
+}
 
-class Property:
-    def __init__(self, var_name: str, name: str, clazz, **kwargs):
-        self.var_name = var_name
-        self.name = name
-        self.property = clazz(name=name, **kwargs)
+ERROR = {"ERROR"}
+INFO = {"INFO"}
+
+CANCELLED = {"CANCELLED"}
+FINISHED = {"FINISHED"}
 
 COLLISION_TYPES = (
     ("AREA",            "Area",         "Area 3D node"          ),
@@ -15,49 +20,154 @@ COLLISION_TYPES = (
     ("CHARACTER_BODY",  "CharacterBody", "CharacterBody3D node" )
 )
 
-has_collision = Property(
-    "has_collision",
-    "Has Collision",
-    bpy.props.BoolProperty,
-    default=False
-)
+class GOBLEND_GeneralProperties(bpy.types.PropertyGroup):
+    exported: bpy.props.BoolProperty(
+        name="Exported",
+        default=True
+    )
 
-collision_type = Property(
-    "collision_type",
-    "Collision Type",
-    bpy.props.EnumProperty,
-    items=COLLISION_TYPES,
-    default="STATIC_BODY"
-)
+class GOBLEND_CollisionProperties(bpy.types.PropertyGroup):
+    has_collision: bpy.props.BoolProperty(
+        name="Has Collision",
+        default=False
+    )
+    collision_type: bpy.props.EnumProperty(
+        name="Collision Type",
+        items=COLLISION_TYPES,
+        default="STATIC_BODY"
+    )
 
-class GOBLEND_PT_GodotSettings(bpy.types.Panel):
-    bl_label = "Godot Settings"
-    bl_idname = "GOBLEND_PT_GodotSettings"
+class GOBLEND_ExportProperties(bpy.types.PropertyGroup):
+    export_path: bpy.props.StringProperty(
+        name="Export Path",
+        subtype="DIR_PATH"
+    )
+
+class GOBLEND_ObjectProperties(bpy.types.PropertyGroup):
+    general: bpy.props.PointerProperty(type=GOBLEND_GeneralProperties)
+    collision: bpy.props.PointerProperty(type=GOBLEND_CollisionProperties)
+
+class GOBLEND_CollectionProperties(bpy.types.PropertyGroup):
+    general: bpy.props.PointerProperty(type=GOBLEND_GeneralProperties)
+    export: bpy.props.PointerProperty(type=GOBLEND_ExportProperties)
+
+
+def collection_resolve_export_path(collection):
+    root = bpy.context.scene.collection
+    return _export_path_walk_collection_tree(root.goblend.export.export_path, root, collection)
+
+def _export_path_walk_collection_tree(inherited_path: str, current, target):
+    if current == target:
+        return inherited_path
+    
+    for child in current.children:
+        current_path = child.goblend.export.export_path or inherited_path
+
+        result = _export_path_walk_collection_tree(current_path, child, target)
+        if result:
+            return result
+    
+    return ""
+
+
+
+class GOBLEND_OT_ExportCollection(bpy.types.Operator):
+    bl_idname = "goblend.export_collection"
+    bl_label = "Export"
+
+    def execute(self, context):
+        collection = context.collection
+        if collection is None:
+            self.report(ERROR, "No collection selected")
+            return CANCELLED
+
+        export_path = collection_resolve_export_path(collection)
+        self.report(INFO, f"exported in: {export_path}")
+
+        return FINISHED
+
+class GOBLEND_PT_GeneralSettings:
     bl_space_type = "PROPERTIES"
     bl_region_type = "WINDOW"
-    bl_context = "object"
+    bl_label = "Godot Settings"
+    bl_idname = "GOBLEND_PT_GeneralSettings"
 
     def draw(self, context):
         layout = self.layout
+        target = self.get_target(context)
+        if target is None: return
+
+        props = target.goblend
+        layout.prop(props.general, "exported")
+        layout.separator()
+    
+    def get_target(self, context):
+        if context.object: return context.object
+        if context.collection: return context.collection
+        return None
+
+def gs_idname(name: str) -> str:
+    return f"{GOBLEND_PT_GeneralSettings.bl_idname}_{name}"
+
+class GOBLEND_PT_ObjectSettings(GOBLEND_PT_GeneralSettings, bpy.types.Panel):
+    bl_idname = gs_idname("Object")
+    bl_context = "object"
+
+    def draw(self, context):
+        GOBLEND_PT_GeneralSettings.draw(self, context)
+
+        layout = self.layout
         obj = context.object
 
-        layout.prop(obj, has_collision.var_name)
+        props = obj.goblend
+
+        layout.prop(props.collision, "has_collision")
 
         row = layout.row()
-        row.enabled = obj.has_collision
-        row.prop(obj, collision_type.var_name)
+        row.enabled = props.collision.has_collision
+        row.prop(props.collision, "collision_type")
+
+class GOBLEND_PT_CollectionSettings(GOBLEND_PT_GeneralSettings, bpy.types.Panel):
+    bl_idname = gs_idname("Collection")
+    bl_context = "collection"
+
+    def draw(self, context):
+        GOBLEND_PT_GeneralSettings.draw(self, context)
+
+        layout = self.layout
+        col = context.collection
+
+        props = col.goblend
+
+        layout.prop(props.export, "export_path")
+        layout.operator("goblend.export_collection", text="Export")
+
+classes = (
+    GOBLEND_GeneralProperties,
+    GOBLEND_CollisionProperties,
+    GOBLEND_ExportProperties,
+
+    GOBLEND_ObjectProperties,
+    GOBLEND_CollectionProperties,
+    
+    GOBLEND_PT_ObjectSettings,
+    GOBLEND_PT_CollectionSettings,
+    GOBLEND_OT_ExportCollection
+)
 
 def register():
-    bpy.types.Object.has_collision = has_collision.property
-    bpy.types.Object.collision_type = collision_type.property
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
-    bpy.utils.register_class(GOBLEND_PT_GodotSettings)
+    bpy.types.Object.goblend = bpy.props.PointerProperty(type=GOBLEND_ObjectProperties)
+    bpy.types.Collection.goblend = bpy.props.PointerProperty(type=GOBLEND_CollectionProperties)
 
 def unregister():
-    del bpy.types.Object.has_collision
-    del bpy.types.Object.collision_type
+    del bpy.types.Object.goblend
+    del bpy.types.Collection.goblend
     
-    bpy.utils.unregister_class(GOBLEND_PT_GodotSettings)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     
 if __name__ == "__main__":
     register()
--- properties.py
+++
@@ -1,5 +0,0 @@
-class Property:
-    def __init__(self, var_name: str, name: str, clazz, **kwargs):
-        self.var_name = var_name
-        self.name = name
-        self.property = clazz(name=name, **kwargs)
